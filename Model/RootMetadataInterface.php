pamil/Sylius

[Metadata] Introduced initial metadata models

src/Sylius/Component/Seo/Model/RootMetadataInterface.php
@@ -1,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Seo\Model;
+
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+interface RootMetadataInterface
+{
+    /**
+     * @return mixed
+     */
+    public function getId();
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key);
+
+    /**
+     * @return string
+     */
+    public function getKey();
+
+    /**
+     * @param MetadataInterface $metadata
+     */
+    public function setMetadata(MetadataInterface $metadata);
+
+    /**
+     * @return MetadataInterface
+     */
+    public function getMetadata();
+
+    /**
+     * @param RootMetadataInterface $rootMetadata
+     */
+    public function setParent(RootMetadataInterface $rootMetadata);
+
+    /**
+     * @return RootMetadataInterface|null
+     */
+    public function getParent();
+
+    /**
+     * @return boolean
+     */
+    public function hasParent();
+
+    /**
+     * @param RootMetadataInterface $rootMetadata
+     */
+    public function addChild(RootMetadataInterface $rootMetadata);
+
+    /**
+     * @param RootMetadataInterface $rootMetadata
+     */
+    public function removeChild(RootMetadataInterface $rootMetadata);
+
+    /**
+     * @return Collection|RootMetadataInterface[]
+     */
+    public function getChildren();
+}

src/Sylius/Component/Seo/Model/Twitter/AbstractCard.php
@@ -1,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Seo\Model\Twitter;
+
+use Sylius\Component\Seo\Model\AbstractMetadata;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+abstract class AbstractCard extends AbstractMetadata implements CardInterface
+{
+    /**
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * @var string
+     */
+    protected $site;
+
+    /**
+     * @var string
+     */
+    protected $siteId;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSite($site)
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSite()
+    {
+        return $this->site;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSiteId($siteId)
+    {
+        $this->siteId = $siteId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSiteId()
+    {
+        return $this->siteId;
+    }
+}

src/Sylius/Component/Seo/Model/Twitter/AppCardInterface.php
@@ -1,0 +1,120 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Seo\Model\Twitter;
+
+/**
+ * @see https://dev.twitter.com/cards/types/app
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+interface AppCardInterface extends CardInterface
+{
+    /**
+     * @param string $description
+     */
+    public function setDescription($description);
+
+    /**
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * @param string $appNameIphone
+     */
+    public function setAppNameIphone($appNameIphone);
+
+    /**
+     * @return string
+     */
+    public function getAppNameIphone();
+
+    /**
+     * @param string $appIdIphone
+     */
+    public function setAppIdIphone($appIdIphone);
+
+    /**
+     * @return string
+     */
+    public function getAppIdIphone();
+
+    /**
+     * @param string $appUrlIphone
+     */
+    public function setAppUrlIphone($appUrlIphone);
+
+    /**
+     * @return string
+     */
+    public function getAppUrlIphone();
+
+    /**
+     * @param string $appNameIpad
+     */
+    public function setAppNameIpad($appNameIpad);
+
+    /**
+     * @return string
+     */
+    public function getAppNameIpad();
+
+    /**
+     * @param string $appIdIpad
+     */
+    public function setAppIdIpad($appIdIpad);
+
+    /**
+     * @return string
+     */
+    public function getAppIdIpad();
+
+    /**
+     * @param string $appUrlIpad
+     */
+    public function setAppUrlIpad($appUrlIpad);
+
+    /**
+     * @return string
+     */
+    public function getAppUrlIpad();
+
+    /**
+     * @param string $appNameGooglePlay
+     */
+    public function setAppNameGooglePlay($appNameGooglePlay);
+
+    /**
+     * @return string
+     */
+    public function getAppNameGooglePlay();
+
+    /**
+     * @param string $appIdGooglePlay
+     */
+    public function setAppIdGooglePlay($appIdGooglePlay);
+
+    /**
+     * @return string
+     */
+    public function getAppIdGooglePlay();
+
+    /**
+     * @param string $appUrlGooglePlay
+     */
+    public function setAppUrlGooglePlay($appUrlGooglePlay);
+
+    /**
+     * @return string
+     */
+    public function getAppUrlGooglePlay();
+}

src/Sylius/Component/Seo/Model/Twitter/CardInterface.php
@@ -1,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Seo\Model\Twitter;
+
+use Sylius\Component\Seo\Model\MetadataInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+interface CardInterface extends MetadataInterface
+{
+    /**
+     * The twitter:card property.
+     *
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * The twitter:site property.
+     *
+     * @param string $site
+     */
+    public function setSite($site);
+
+    /**
+     * The twitter:site property.
+     *
+     * @return string
+     */
+    public function getSite();
+
+    /**
+     * The twitter:site:id property.
+     *
+     * @param string $siteId
+     */
+    public function setSiteId($siteId);
+
+    /**
+     * The twitter:site:id property.
+     *
+     * @return string
+     */
+    public function getSiteId();
+}

src/Sylius/Component/Seo/Model/Twitter/SummaryCard.php
@@ -1,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Seo\Model\Twitter;
+
+/**
+ * {@inheritdoc}
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryCard extends AbstractCard implements SummaryCardInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $type = 'summary';
+
+    /**
+     * @var string
+     */
+    protected $creatorId;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var string
+     */
+    protected $image;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize()
+    {
+        return json_encode([
+            $this->site,
+            $this->siteId,
+            $this->creatorId,
+            $this->title,
+            $this->description,
+            $this->image,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->site,
+            $this->siteId,
+            $this->creatorId,
+            $this->title,
+            $this->description,
+            $this->image,
+        ) = json_decode($serialized, true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreatorId($creatorId)
+    {
+        $this->creatorId = $creatorId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatorId()
+    {
+        return $this->creatorId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}

src/Sylius/Component/Seo/spec/Model/RootMetadataSpec.php
@@ -1,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Seo\Model;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Seo\Model\MetadataInterface;
+use Sylius\Component\Seo\Model\RootMetadataInterface;
+
+/**
+ * @mixin \Sylius\Component\Seo\Model\RootMetadata
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class RootMetadataSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Component\Seo\Model\RootMetadata');
+    }
+
+    function it_implements_Root_Metadata_interface()
+    {
+        $this->shouldImplement('Sylius\Component\Seo\Model\RootMetadataInterface');
+    }
+
+    function it_has_no_id_by_default()
+    {
+        $this->getId()->shouldReturn(null);
+    }
+
+    function it_has_key()
+    {
+        $this->getKey()->shouldReturn(null);
+
+        $this->setKey('unique_key:42');
+        $this->getKey()->shouldReturn('unique_key:42');
+    }
+
+    function it_has_metadata(MetadataInterface $metadata)
+    {
+        $this->getMetadata()->shouldReturn(null);
+
+        $this->setMetadata($metadata);
+        $this->getMetadata()->shouldReturn($metadata);
+    }
+
+    function it_has_parent(RootMetadataInterface $rootMetadata)
+    {
+        $this->getParent()->shouldReturn(null);
+        $this->hasParent()->shouldReturn(false);
+
+        $this->setParent($rootMetadata);
+
+        $this->getParent()->shouldReturn($rootMetadata);
+        $this->hasParent()->shouldReturn(true);
+    }
+
+    function it_has_children(RootMetadataInterface $rootMetadata)
+    {
+        $this->getChildren()->shouldHaveCount(0);
+
+        $this->addChild($rootMetadata);
+
+        $this->getChildren()->shouldHaveCount(1);
+
+        $this->removeChild($rootMetadata);
+
+        $this->getChildren()->shouldHaveCount(0);
+    }
+}

src/Sylius/Component/Seo/spec/Model/Twitter/SummaryCardSpec.php
@@ -1,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Seo\Model\Twitter;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Seo\Model\MetadataInterface;
+use Sylius\Component\Seo\Model\Twitter\SummaryCard;
+
+/**
+ * @mixin \Sylius\Component\Seo\Model\Twitter\SummaryCard
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryCardSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Component\Seo\Model\Twitter\SummaryCard');
+    }
+
+    function it_implements_Summary_Card_interface()
+    {
+        $this->shouldImplement('Sylius\Component\Seo\Model\Twitter\SummaryCardInterface');
+    }
+
+    function it_is_serializable()
+    {
+        $this->setTitle('Lorem ipsum');
+
+        $that = unserialize(serialize($this));
+
+        $that->getTitle()->shouldReturn('Lorem ipsum');
+    }
+
+    function it_is_mergable_with_same_class_object()
+    {
+        $metadata = new SummaryCard();
+        $metadata->setTitle('Lorem ipsum');
+        $metadata->setDescription('Lorem ipsum dolor sit amet');
+
+        $this->setTitle('Death Star');
+        $this->setSite('@pamilme');
+
+        $this->merge($metadata);
+
+        $this->getTitle()->shouldReturn('Death Star');
+        $this->getDescription()->shouldReturn('Lorem ipsum dolor sit amet');
+        $this->getSite()->shouldReturn('@pamilme');
+    }
+
+    function it_can_not_merge_with_different_class_object(MetadataInterface $metadata)
+    {
+        $this->shouldThrow('\InvalidArgumentException')->duringMerge($metadata);
+    }
+
+    function it_has_correct_type()
+    {
+        $this->getType()->shouldReturn('summary');
+    }
+
+    function it_has_site()
+    {
+        $this->getSite()->shouldReturn(null);
+
+        $this->setSite('@pamilme');
+        $this->getSite()->shouldReturn('@pamilme');
+    }
+
+    function it_has_site_id()
+    {
+        $this->getSiteId()->shouldReturn(null);
+
+        $this->setSiteId('753686598');
+        $this->getSiteId()->shouldReturn('753686598');
+    }
+
+    function it_has_creator_id()
+    {
+        $this->getCreatorId()->shouldReturn(null);
+
+        $this->setCreatorId('753686598');
+        $this->getCreatorId()->shouldReturn('753686598');
+    }
+
+    function it_has_title()
+    {
+        $this->getTitle()->shouldReturn(null);
+
+        $this->setTitle('Lorem ipsum');
+        $this->getTitle()->shouldReturn('Lorem ipsum');
+    }
+
+    function it_has_description()
+    {
+        $this->getDescription()->shouldReturn(null);
+
+        $this->setDescription('Lorem ipsum dolor sit amet');
+        $this->getDescription()->shouldReturn('Lorem ipsum dolor sit amet');
+    }
+
+    function it_has_image()
+    {
+        $this->getImage()->shouldReturn(null);
+
+        $this->setImage('http://sylius.org/assets/img/logo.png');
+        $this->getImage()->shouldReturn('http://sylius.org/assets/img/logo.png');
+    }
+}

Sylius/Metadata

Model/RootMetadataInterface.php
@@ -1,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Metadata\Model;
+
+use Doctrine\Common\Collections\Collection;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+interface RootMetadataInterface
+{
+    /**
+     * @return mixed
+     */
+    public function getId();
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key);
+
+    /**
+     * @return string
+     */
+    public function getKey();
+
+    /**
+     * @param MetadataInterface $metadata
+     */
+    public function setMetadata(MetadataInterface $metadata);
+
+    /**
+     * @return MetadataInterface
+     */
+    public function getMetadata();
+
+    /**
+     * @param RootMetadataInterface $rootMetadata
+     */
+    public function setParent(RootMetadataInterface $rootMetadata);
+
+    /**
+     * @return RootMetadataInterface|null
+     */
+    public function getParent();
+
+    /**
+     * @return boolean
+     */
+    public function hasParent();
+
+    /**
+     * @param RootMetadataInterface $rootMetadata
+     */
+    public function addChild(RootMetadataInterface $rootMetadata);
+
+    /**
+     * @param RootMetadataInterface $rootMetadata
+     */
+    public function removeChild(RootMetadataInterface $rootMetadata);
+
+    /**
+     * @return Collection|RootMetadataInterface[]
+     */
+    public function getChildren();
+}

Model/Twitter/AbstractCard.php
@@ -1,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Metadata\Model\Twitter;
+
+use Sylius\Component\Metadata\Model\AbstractMetadata;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+abstract class AbstractCard extends AbstractMetadata implements CardInterface
+{
+    /**
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * @var string
+     */
+    protected $site;
+
+    /**
+     * @var string
+     */
+    protected $siteId;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSite($site)
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSite()
+    {
+        return $this->site;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSiteId($siteId)
+    {
+        $this->siteId = $siteId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSiteId()
+    {
+        return $this->siteId;
+    }
+}

Model/Twitter/AppCardInterface.php
@@ -1,0 +1,120 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Metadata\Model\Twitter;
+
+/**
+ * @see https://dev.twitter.com/cards/types/app
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+interface AppCardInterface extends CardInterface
+{
+    /**
+     * @param string $description
+     */
+    public function setDescription($description);
+
+    /**
+     * @return string
+     */
+    public function getDescription();
+
+    /**
+     * @param string $appNameIphone
+     */
+    public function setAppNameIphone($appNameIphone);
+
+    /**
+     * @return string
+     */
+    public function getAppNameIphone();
+
+    /**
+     * @param string $appIdIphone
+     */
+    public function setAppIdIphone($appIdIphone);
+
+    /**
+     * @return string
+     */
+    public function getAppIdIphone();
+
+    /**
+     * @param string $appUrlIphone
+     */
+    public function setAppUrlIphone($appUrlIphone);
+
+    /**
+     * @return string
+     */
+    public function getAppUrlIphone();
+
+    /**
+     * @param string $appNameIpad
+     */
+    public function setAppNameIpad($appNameIpad);
+
+    /**
+     * @return string
+     */
+    public function getAppNameIpad();
+
+    /**
+     * @param string $appIdIpad
+     */
+    public function setAppIdIpad($appIdIpad);
+
+    /**
+     * @return string
+     */
+    public function getAppIdIpad();
+
+    /**
+     * @param string $appUrlIpad
+     */
+    public function setAppUrlIpad($appUrlIpad);
+
+    /**
+     * @return string
+     */
+    public function getAppUrlIpad();
+
+    /**
+     * @param string $appNameGooglePlay
+     */
+    public function setAppNameGooglePlay($appNameGooglePlay);
+
+    /**
+     * @return string
+     */
+    public function getAppNameGooglePlay();
+
+    /**
+     * @param string $appIdGooglePlay
+     */
+    public function setAppIdGooglePlay($appIdGooglePlay);
+
+    /**
+     * @return string
+     */
+    public function getAppIdGooglePlay();
+
+    /**
+     * @param string $appUrlGooglePlay
+     */
+    public function setAppUrlGooglePlay($appUrlGooglePlay);
+
+    /**
+     * @return string
+     */
+    public function getAppUrlGooglePlay();
+}

Model/Twitter/CardInterface.php
@@ -1,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Metadata\Model\Twitter;
+
+use Sylius\Component\Metadata\Model\MetadataInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+interface CardInterface extends MetadataInterface
+{
+    /**
+     * The twitter:card property.
+     *
+     * @return string
+     */
+    public function getType();
+
+    /**
+     * The twitter:site property.
+     *
+     * @param string $site
+     */
+    public function setSite($site);
+
+    /**
+     * The twitter:site property.
+     *
+     * @return string
+     */
+    public function getSite();
+
+    /**
+     * The twitter:site:id property.
+     *
+     * @param string $siteId
+     */
+    public function setSiteId($siteId);
+
+    /**
+     * The twitter:site:id property.
+     *
+     * @return string
+     */
+    public function getSiteId();
+}

Model/Twitter/SummaryCard.php
@@ -1,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Metadata\Model\Twitter;
+
+/**
+ * {@inheritdoc}
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryCard extends AbstractCard implements SummaryCardInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $type = 'summary';
+
+    /**
+     * @var string
+     */
+    protected $creatorId;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $description;
+
+    /**
+     * @var string
+     */
+    protected $image;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize()
+    {
+        return json_encode([
+            $this->site,
+            $this->siteId,
+            $this->creatorId,
+            $this->title,
+            $this->description,
+            $this->image,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->site,
+            $this->siteId,
+            $this->creatorId,
+            $this->title,
+            $this->description,
+            $this->image,
+        ) = json_decode($serialized, true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCreatorId($creatorId)
+    {
+        $this->creatorId = $creatorId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatorId()
+    {
+        return $this->creatorId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}

spec/Model/RootMetadataSpec.php
@@ -1,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Metadata\Model;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Metadata\Model\MetadataInterface;
+use Sylius\Component\Metadata\Model\RootMetadataInterface;
+
+/**
+ * @mixin \Sylius\Component\Metadata\Model\RootMetadata
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class RootMetadataSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Component\Metadata\Model\RootMetadata');
+    }
+
+    function it_implements_Root_Metadata_interface()
+    {
+        $this->shouldImplement('Sylius\Component\Metadata\Model\RootMetadataInterface');
+    }
+
+    function it_has_no_id_by_default()
+    {
+        $this->getId()->shouldReturn(null);
+    }
+
+    function it_has_key()
+    {
+        $this->getKey()->shouldReturn(null);
+
+        $this->setKey('unique_key:42');
+        $this->getKey()->shouldReturn('unique_key:42');
+    }
+
+    function it_has_metadata(MetadataInterface $metadata)
+    {
+        $this->getMetadata()->shouldReturn(null);
+
+        $this->setMetadata($metadata);
+        $this->getMetadata()->shouldReturn($metadata);
+    }
+
+    function it_has_parent(RootMetadataInterface $rootMetadata)
+    {
+        $this->getParent()->shouldReturn(null);
+        $this->hasParent()->shouldReturn(false);
+
+        $this->setParent($rootMetadata);
+
+        $this->getParent()->shouldReturn($rootMetadata);
+        $this->hasParent()->shouldReturn(true);
+    }
+
+    function it_has_children(RootMetadataInterface $rootMetadata)
+    {
+        $this->getChildren()->shouldHaveCount(0);
+
+        $this->addChild($rootMetadata);
+
+        $this->getChildren()->shouldHaveCount(1);
+
+        $this->removeChild($rootMetadata);
+
+        $this->getChildren()->shouldHaveCount(0);
+    }
+}

spec/Model/Twitter/SummaryCardSpec.php
@@ -1,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Metadata\Model\Twitter;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Metadata\Model\MetadataInterface;
+use Sylius\Component\Metadata\Model\Twitter\SummaryCard;
+
+/**
+ * @mixin \Sylius\Component\Metadata\Model\Twitter\SummaryCard
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+class SummaryCardSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Component\Metadata\Model\Twitter\SummaryCard');
+    }
+
+    function it_implements_Summary_Card_interface()
+    {
+        $this->shouldImplement('Sylius\Component\Metadata\Model\Twitter\SummaryCardInterface');
+    }
+
+    function it_is_serializable()
+    {
+        $this->setTitle('Lorem ipsum');
+
+        $that = unserialize(serialize($this));
+
+        $that->getTitle()->shouldReturn('Lorem ipsum');
+    }
+
+    function it_is_mergable_with_same_class_object()
+    {
+        $metadata = new SummaryCard();
+        $metadata->setTitle('Lorem ipsum');
+        $metadata->setDescription('Lorem ipsum dolor sit amet');
+
+        $this->setTitle('Death Star');
+        $this->setSite('@pamilme');
+
+        $this->merge($metadata);
+
+        $this->getTitle()->shouldReturn('Death Star');
+        $this->getDescription()->shouldReturn('Lorem ipsum dolor sit amet');
+        $this->getSite()->shouldReturn('@pamilme');
+    }
+
+    function it_can_not_merge_with_different_class_object(MetadataInterface $metadata)
+    {
+        $this->shouldThrow('\InvalidArgumentException')->duringMerge($metadata);
+    }
+
+    function it_has_correct_type()
+    {
+        $this->getType()->shouldReturn('summary');
+    }
+
+    function it_has_site()
+    {
+        $this->getSite()->shouldReturn(null);
+
+        $this->setSite('@pamilme');
+        $this->getSite()->shouldReturn('@pamilme');
+    }
+
+    function it_has_site_id()
+    {
+        $this->getSiteId()->shouldReturn(null);
+
+        $this->setSiteId('753686598');
+        $this->getSiteId()->shouldReturn('753686598');
+    }
+
+    function it_has_creator_id()
+    {
+        $this->getCreatorId()->shouldReturn(null);
+
+        $this->setCreatorId('753686598');
+        $this->getCreatorId()->shouldReturn('753686598');
+    }
+
+    function it_has_title()
+    {
+        $this->getTitle()->shouldReturn(null);
+
+        $this->setTitle('Lorem ipsum');
+        $this->getTitle()->shouldReturn('Lorem ipsum');
+    }
+
+    function it_has_description()
+    {
+        $this->getDescription()->shouldReturn(null);
+
+        $this->setDescription('Lorem ipsum dolor sit amet');
+        $this->getDescription()->shouldReturn('Lorem ipsum dolor sit amet');
+    }
+
+    function it_has_image()
+    {
+        $this->getImage()->shouldReturn(null);
+
+        $this->setImage('http://sylius.org/assets/img/logo.png');
+        $this->getImage()->shouldReturn('http://sylius.org/assets/img/logo.png');
+    }
+}